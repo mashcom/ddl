bbd360cde136ec36454458c2160e2fbc15660727
Blessing Mashoko <bmashcom@hotmail.com>
2020-10-29 18:13:14 +0200

    update title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -58,7 +58,7 @@
     <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
         <div class="container">
             <a class="navbar-brand" href="{{ url('licence') }}">
-                ID Admin
+                Digital ID
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                     aria-controls="navbarSupportedContent" aria-expanded="false"
